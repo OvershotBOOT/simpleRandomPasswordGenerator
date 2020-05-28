7c8552cc18fe788b54c6e18593f6928e4ac6d141
OvershotBOOT <tomseymour217@gmail.com>
2020-05-28 14:31:37 -0700

    added functions and variables to .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'h', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialCharacters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
+function generatePassword() {
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   // you can create a function named generatePassword that creates the password
