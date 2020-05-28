794056c63f7ec847c1e121d40ebe6f932afe8a6a
OvershotBOOT <tomseymour217@gmail.com>
2020-05-28 15:05:40 -0700

    Finished caseChange function, added more functions and arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,58 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'h', 'l',
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var specialCharacters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
+var characterArr = [
+    alphabet,
+    numbers,
+    specialCharacters
+  ]
+
+var characterArrayHolder
+
+var caseSwitch = [0, 1]
+var currentCharacter
+var currentPassword
+
+function characterPicker() {
+  if (alphabeticalBool === true) {
+    characterArrayHolder.append(alphabet)
+  }
+  if (numberBool === true) {
+    characterArrayHolder.append(numbers)
+  }
+  if (specialCharBool === true) {
+    characterArrayHolder.append(specialCharacters)
+  }
+}
+
+function caseChange(uppercaseBool) {
+  if (uppercaseBool === true && lowercaseBool === true) {
+    caseSwitch = Math.floor(Math.random() * caseSwitch.length);
+    if (caseSwitch === 1) {
+      currentCharacter.toUpperCase()
+    } 
+    else {
+      currentCharacter.toLowerCase()
+    }
+  }
+  else if (uppercaseBool === true && lowercaseBool === false)  {
+      currentCharacter.toUpperCase()
+  }
+  else {
+    currentCharacter.toLowerCase()
+  }
+}
+
 function generatePassword() {
-  
+  var alphabetBool = prompt('Do you want to have alphabetical characters in your password?');
+  if (alphabeticalBool === true) {
+    var uppercaseBool = prompt('Would you like uppercase characters?')
+    var lowercaseBool = prompt('Would you like lowercase characters?')
+  }
+  var numberBool = prompt('Do you want to have numerical characters in your password?')
+  var specialCharBool = prompt('Do you want to have special characters in your password?')
+
+
 }
 
 // Write password to the #password input
